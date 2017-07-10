75e6275d025c409a2ec9c3bc978654a94468d34f
Lukas Reindl <lukas.reindl@innoactive.de>
2017-07-11 00:15:59 +0200

    hiding working

---
+++ PARTEIPlakate/Assets/Scripts/EventHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class EventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PRIVATE_MEMBER_VARIABLES
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    #endregion // PRIVATE_MEMBER_VARIABLES
+    
+    
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+    private void OnTrackingFound()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnTrackingLost()
+    {
+        transform.position = new Vector3(-400, -1000, -1000);
+    }
+
+}
