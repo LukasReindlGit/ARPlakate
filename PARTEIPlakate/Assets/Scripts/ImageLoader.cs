e2ca2004f29e93419cf6c43a1ac044ee038b0089
Lukas Reindl <lukas.reindl@innoactive.de>
2017-07-11 00:42:56 +0200

    loading images from config url

---
+++ PARTEIPlakate/Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class ImageLoader : MonoBehaviour
+{
+
+    [SerializeField]
+    TextAsset configFile;
+    string[] config;
+
+    [SerializeField]
+    Renderer visual;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitTexture();
+    }
+    
+    private void InitTexture()
+    {
+        LoadConfig();
+        StartCoroutine(LoadTextureFromURL(GetRandomURLFromConfig()));
+    }
+
+    private string GetRandomURLFromConfig(int ttl = 100)
+    {
+        if (ttl <= 0)
+            return "";
+
+        string url = config[UnityEngine.Random.Range(0, config.Length)];
+
+        if (url.Length < 5)
+            url = GetRandomURLFromConfig(ttl-1);
+
+        return url;
+    }
+
+
+    IEnumerator LoadTextureFromURL(string url)
+    {
+        Debug.Log("Trying: URL: " + url);
+
+        Texture2D tex;
+        tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+        WWW www = new WWW(url);
+        yield return www;
+        www.LoadImageIntoTexture(tex);
+        if (tex != null)
+        {
+            visual.material.mainTexture = tex;
+        }
+
+    }
+
+    private void LoadConfig()
+    {
+        string fs = configFile.text;
+        config = Regex.Split(fs, "\n|\r|\r\n");
+    }
+}
