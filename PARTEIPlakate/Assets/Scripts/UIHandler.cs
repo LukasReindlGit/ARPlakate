042bce254c9e1bbab4d4decf485d5594bccdff4d
Lukas Reindl <lukas.reindl@innoactive.de>
2017-07-11 02:12:28 +0200

    basic ui working

---
+++ PARTEIPlakate/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+
+
+    [SerializeField]
+    int levelToGoOnEsc = 0;
+
+    [SerializeField]
+    GameObject notWorkingmenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (levelToGoOnEsc >= 0)
+            {
+                GoToScene(levelToGoOnEsc);
+            }
+            else
+            {
+                QuitApplication();
+            }
+        }
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void NotWorkingButton()
+    {
+        notWorkingmenu.SetActive(true);
+    }
+
+    public void GoToScene(int i)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(i);
+    }
+}
