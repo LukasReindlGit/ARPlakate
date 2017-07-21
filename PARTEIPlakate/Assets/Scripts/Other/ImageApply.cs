12abc3358cdca97a6d9434df4a7b04340ff57c15
Lukas Reindl <lukas.reindl@innoactive.de>
2017-07-21 22:57:40 +0200

    only load texture if needed

---
+++ PARTEIPlakate/Assets/Scripts/Other/ImageApply.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageApply : MonoBehaviour, ITrackableEventHandler {
+
+    [SerializeField]
+    Renderer visual;
+
+    bool gotTexture = false;
+
+    void Start()
+    {
+        if (visual == null)
+        {
+            visual = GetComponent<Renderer>();
+        }
+        
+        GetComponentInParent<ImageTargetBehaviour>().RegisterTrackableEventHandler(this);
+        
+    }        
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        Debug.Log("NEW STATE: " + newStatus.ToString());
+
+        if(!gotTexture && newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            InitTexture();
+        }
+    }
+
+    private void InitTexture()
+    {
+        visual.material.mainTexture = TextureManager.GetRandomTexture();
+        gotTexture = true;
+    }
+}
---
+++ PARTEIPlakate/Assets/Scripts/Other/TextureManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureManager : MonoBehaviour {
+
+    public static TextureManager Instance;
+
+    [SerializeField]
+    Texture2D[] fallbackTextures;
+
+    List<Texture2D> allTextures= new List<Texture2D>();
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Init textures
+        foreach(var tex in fallbackTextures)
+        {
+            allTextures.Add(tex);
+        }
+    }
+
+    public static Texture2D GetRandomTexture()
+    {
+        return Instance.GetRandomTextureFromList();
+    }
+
+    private Texture2D GetRandomTextureFromList()
+    {
+        return allTextures[UnityEngine.Random.Range(0, allTextures.Count)];
+    }
+}
